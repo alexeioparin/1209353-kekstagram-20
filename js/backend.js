002a150c0921029afe8070a328a73fce0e7074ef
Spin <alexeioparin@gmail.com>
2020-06-30 18:32:16 +0300

    Работа с сетью

---
+++ js/backend.js
@@ -0,0 +1,63 @@
+'use strict';
+
+window.backend = (function () {
+
+  var StatusCode = {
+    OK: 200
+  };
+
+  // создание запроса, загрузка информации с сервера
+
+  var load = function (onLoad, onError) {
+    var TIMEOUT_IN_MS = 10000;
+    var URL = 'https://javascript.pages.academy/kekstagram/data';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', URL);
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.timeout = TIMEOUT_IN_MS;
+    xhr.send();
+  };
+
+  /* var save = function (data, onLoad, onError) {
+    var TIMEOUT_IN_MS = 10000;
+    var URL = 'https://javascript.pages.academy/code-and-magick';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.open('POST', URL);
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус отправки: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Загрузка не успела выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.send(data);
+  }; */
+
+  return {
+    load: load,
+  //  save: save,
+  };
+})();
--- js/main.js
+++ js/main.js
@@ -63,17 +63,32 @@ window.main = (function () {
     return userElement;
   };
 
-  /* Отрисовка DOM элемента */
+  // Добавление изображений с сервера
 
   var setUserElement = function (userPhotoList) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < userPhotoList.length; i++) {
       fragment.appendChild(getUserElement(userPhotoList[i]));
     }
-    return fragment;
+    photoList.appendChild(fragment);
   };
 
-  photoList.appendChild(setUserElement(getUserPhotos()));
+  // Отображение окна ошибок
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(setUserElement, errorHandler);
+
 
   // bigPicture.classList.remove('hidden');
 
